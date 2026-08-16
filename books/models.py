2bb03d685b85b91d3c2181a8c6f9d7fd44bed676
rezagh100 <gholamhasanireza@gmail.com>
2026-08-16 20:53:37 +0330

    Add book models and initial migration

--- books/models.py
+++ books/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=250)
+    bio = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey(Author,on_delete=models.RESTRICT,related_name='books')
+    category =  models.ForeignKey(Category,on_delete=models.RESTRICT)
+    isbn = models.CharField(max_length=100,unique=True)
+    total_copies = models.PositiveIntegerField()
+    available_copies = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return self.title
+    
